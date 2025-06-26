54f7563a1a4635326d8dfd297f7f0140741ed39e
wjortiz <67480834+TheWillyXD@users.noreply.github.com>
2025-06-25 19:32:21 -0500

    Update main.cpp

--- Esp32/src/main.cpp
+++ Esp32/src/main.cpp
@@ -7,21 +7,27 @@ unsigned long tiempo = 0;
 
 void setup() {
   iniciarDrivers();
-  avanzar(512);
+  avanzar(1023);
+  
+  
   tiempo = millis();
 }
 
 void loop() {
-  
+  direccionCentro(); // posición inicial
+  //girarIzquierdaServos();
+  //girarDerechaServos();
   if (millis() - tiempo >= 2000) {
 
     detener();            
     delay(1000);           
 
     if (adelante) {
-      retroceder(512);  //Velocidad 512/50%  768/75%   1023/100%
+       
+      retroceder(1023);  //Velocidad 512/50%  768/75%   1023/100%
     } else {
-      avanzar(512);     
+      
+      avanzar(1023);     
     }
     adelante = !adelante;
     tiempo = millis();  
